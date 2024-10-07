fc2215979e73cec3f10491784c96613e87025fde
Akshar <99510948+SOKONDU@users.noreply.github.com>
2024-10-07 18:33:40 +0000

    done

--- src/main/java/org/nhs/app/App.java
+++ src/main/java/org/nhs/app/App.java
@@ -1,8 +1,93 @@
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
 
-package org.nhs.app;
+/*public class App
+{
+    
+    public static void main(String[] args)
+    {
+    mystery();
+    }
 
-// Your code goes here
+    public static void mystery()
+    {
+    int foo = 3;
+    for(int num = 0; foo > num; num += 2)
+    {
+        foo++;
+        System.out.println(foo + " " + num + " ");
+    }
+    }
+    
+}*/
+
+/*public class App
+{
+    public static void main(String[] args)
+    {
+        for(int n = 1; n <=5; n++)
+        {
+            for(int counter = 1; counter <=4; counter++)
+            {
+                System.out.print(n);
+            }
+            System.out.println();
+        }
+    }
+}*/
+
+public class App
+{
+    public static void main(String[] args)
+    {
+       first();
+       System.out.println();
+       second();
+       System.out.println();
+       third();
+    }
+
+    public static void first()
+    {
+        for(int i = 1; i<= 5; i++)
+        {
+            for(int j = 1; j <= i; j++)
+            {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void second()
+    {
+        for(int n = 1; n<= 5; n++)
+        {
+            for(int x = 1; x <= 5-n; x++)
+            {
+                System.out.print(".");
+            }
+            for(int y = 1; y <= n; y++)
+            {
+                System.out.print(n);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void third()
+    {
+        for(int l = 1; l <= 5; l++)
+        {
+            for(int m = 1; m <= 5-l; m++)
+            {
+                System.out.print(".");
+            }
+            System.out.print(l);
+            for(int o = 1; o <= l-1; o++)
+            {
+                System.out.print(".");
+            }
+            System.out.println();
+        }
+    }
+}
+
